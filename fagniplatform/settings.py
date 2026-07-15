0c4c4942fbbef04fddf06e45c6c4c3b3f5625bb0
Désiré Tongbé <desiretongbe@gmail.com>
2026-07-15 11:15:39 +0200

    fix(infra): SECRET_KEY malformee lors du patch precedent (texte imbrique en double au lieu de la vraie valeur de fallback) - corrigee, verifiee avec manage.py check et les tests

--- fagniplatform/settings.py
+++ fagniplatform/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", 'os.environ.get("SECRET_KEY", \'django-insecure-eb%&9w2erck)qkayjvr@+u6c4)kqxo*06l&*759chphj)t=wty\')')
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-eb%&9w2erck)qkayjvr@+u6c4)kqxo*06l&*759chphj)t=wty")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
